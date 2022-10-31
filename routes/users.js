f9e5b517e1742b50ce3b27eacc573b9e011803d1
Aditi Soni <aditi34soni@gmail.com>
2022-10-31 18:37:13 +0530

    new commit

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,9 @@ const express= require('express');
 
  router.get('/profile',usersController.profile);
  router.get('/posts',postsController.posts);
+ router.get('/sign-up',usersController.signUp);
+ router.get('/sign-in',usersController.signIn);
+ router.post('/create', usersController.create);
+ router.post('/create-session', usersController.createSession);
 
- module.exports =router;
+ module.exports = router;
